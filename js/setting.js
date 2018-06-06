53935f7d2e601f40a40027d5c87d5fcfb7829274
Anton Dev <deanton28@gmail.com>
2018-06-07 02:58:32 +0300

    add hit to target, error lint show-object 21 str

--- js/setting.js
+++ js/setting.js
@@ -2,6 +2,8 @@ window.setting = {};
 
 (function tune(exports) {
   const setup = document.querySelector('.gun__setup');
+  const distanceTarget = setup.querySelector('#target-distance');
+
   exports.speedTarget = setup.querySelector('#target-speed');
   exports.sizeBulletClip = setup.querySelector('#bullet-quantity');
   exports.speedBullet = setup.querySelector('#bullet-speed');
@@ -16,7 +18,11 @@ window.setting = {};
   });
 
   exports.speedTarget.addEventListener('change', () => {
-    // setInterval(window.gun.moveTarget, speedTarget.value);
     exports.speedTarget.blur();
+  });
+
+  distanceTarget.addEventListener('change', () => {
+    window.gun.target.style.left = `${distanceTarget.value}px`;
+    distanceTarget.blur();
   });
 }(window.setting));
--- js/show-object.js
+++ js/show-object.js
@@ -1,11 +1,31 @@
 window.object = {};
 
 (function showObject(exports) {
+  let timerIdTarget = 0;
+
   const moveObjectHorizontal = (object, place) => {
-    const horizontalShift = 10;
+    const getElementSize = (el, attr) => el.getBoundingClientRect()[attr];
+
+    const horizontalShift = 2;
     const objectShift = object.offsetLeft + horizontalShift;
 
-    if (objectShift < 550) {
+    const objectLeft = object.offsetLeft;
+    const objectTop = object.offsetTop;
+    const objectRight = objectLeft + getElementSize(object, 'width');
+    const objectBottom = objectTop + getElementSize(object, 'height');
+    const targetLeft = window.gun.target.offsetLeft;
+    const targetTop = window.gun.target.offsetTop;
+    const targetRight = targetLeft + getElementSize(window.gun.target, 'width');
+    const targetBottom = targetTop + getElementSize(window.gun.target, 'height');
+
+    if (objectRight > targetLeft && objectRight < targetRight && objectBottom > targetTop && objectTop < targetBottom) {
+      window.gun.messageHitTarget.removeAttribute('hidden');
+      window.gun.target.setAttribute('hidden', '');
+      window.gun.messageHitTarget.addEventListener('click', window.gun.restart);
+      document.addEventListener('keydown', window.gun.restart);
+      place.removeChild(object);
+      clearTimeout(timerIdTarget);
+    } else if (objectShift < 550) {
       object.style.left = `${objectShift}px`;
       setTimeout(moveObjectHorizontal, window.setting.speedBullet.value, object, place);
     } else {
@@ -17,11 +37,13 @@ window.object = {};
     let shift = verticalShift;
 
     const moveObjectVertical = () => {
+      const timeout = window.setting.speedTarget.value;
+
       if (object.offsetTop < 3 || object.offsetTop > 38) {
         shift = -shift;
       }
       object.style.top = `${object.offsetTop + shift}px`;
-      setTimeout(moveObjectVertical, window.setting.speedTarget.value, object, verticalShift);
+      timerIdTarget = setTimeout(moveObjectVertical, timeout, object, verticalShift);
     };
 
     moveObjectVertical();
